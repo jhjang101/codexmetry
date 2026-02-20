c1c907d6e34abc993889eabad41dcc4abc671e80
Joon Hee Jang <jhjang101@gmail.com>
2026-02-19 18:51:56 -0600

    FEAT: created login.html - add @login_required in dashboard.py

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template
+from flask_login import login_required
 
 bp = Blueprint('dashboard', __name__)
 
 @bp.route('/')
 @bp.route('/dashboard')
+@login_required 
 def index():
     return render_template('dashboard/dashboard.html')
