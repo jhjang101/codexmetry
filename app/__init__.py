050cd27f624150c519380b4e86603c91fb4d2f43
Joon Hee Jang <jhjang101@gmail.com>
2026-06-17 18:46:04 -0500

    v0.2.1

--- app/__init__.py
+++ app/__init__.py
@@ -14,11 +14,11 @@ from .utils.auth import init_auth_loaders
 """
 CODEXMETRY: The Financial Fortress of Order Management
 ------------------------------------------------------
-Version: 0.2.0
+Version: 0.2.1
 Author:  Joon Hee Jang
 License: MIT
 """
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 
 def create_app():
